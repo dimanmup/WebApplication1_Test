c2741db1fd763d2068c60ee50b74c2de3be93b58
Neomaster <the@neomaster.net>
2022-04-25 12:57:52 +0300

    1.1.0.0 Telerik, Butstrap, Ajax validation scripts

--- WebApplication1_Test/Program.cs
+++ WebApplication1_Test/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Telerik.
+builder.Services.AddKendo();
+
+// Отмена автоматического перевода JSON в camal-case в HTTP-response.
+builder.Services.AddMvc()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
